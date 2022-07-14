163de5ff1a5a5dfb79d35165b400b0bb683fc45e
AzizAbd <91793452+AzizAbd@users.noreply.github.com>
2022-07-14 16:15:00 +0200

    utiliser Components

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const page = (
-  <div>
-    <img src="./React-icon.svg.png" width="100px" />
-    <h1>fun facts about react</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of entrprise apps, including mobile apps</li>
-    </ul>
-  </div>
-);
-
-ReactDOM.render(page, document.getElementById("root"));
+function TemporaryName() {
+  return (
+    <div>
+      <img src="./React-icon.svg.png" width="100px" />
+      <h1>fun facts about react</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of entrprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  );
+}
+ReactDOM.render(<TemporaryName />, document.getElementById("root"));
